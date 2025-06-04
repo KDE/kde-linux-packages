b23d2515acccb3962f84e857cea05ae31edfa7a7
Hadi Chokr <hadichokr@icloud.com>
2025-06-04 17:06:59 +0000

    Edit make-pkgbuilds.py

--- make-pkgbuilds.py
+++ make-pkgbuilds.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+
 # kde-builder should already be in the path
 import datetime
 import logging.config
