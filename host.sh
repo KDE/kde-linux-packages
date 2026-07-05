78f2099d62e59cab4261c2ee7928b7e9baf3adaa
Harald Sitter <sitter@kde.org>
2026-07-05 16:29:50 +0200

    host.sh: reorder
    so my old eyes don't copy the wrong line when copy pasting something

--- host.sh
+++ host.sh
@@ -4,15 +4,15 @@
 
 set -eux
 
+[ -d /tmp/host ] || mkdir /tmp/host
+[ -x /tmp/host/kde-builder-logs ] || cp caddy /tmp/host/kde-builder-logs
+# Make sure the build namespace can write into the logs directory
+chmod 777 /tmp/host/kde-builder-logs
+
 if [ ! -x caddy ]; then
     wget https://github.com/caddyserver/caddy/releases/download/v2.11.4/caddy_2.11.4_linux_amd64.tar.gz
     tar --extract --file=caddy_2.11.4_linux_amd64.tar.gz caddy
 fi
-
-[ -d /tmp/host ] || mkdir /tmp/host
 [ -x /tmp/host/caddy ] || cp caddy /tmp/host/caddy
-[ -x /tmp/host/kde-builder-logs ] || cp caddy /tmp/host/kde-builder-logs
-# Make sure the build namespace can write into the logs directory
-chmod 777 /tmp/host/kde-builder-logs
 
 exec ./caddy run
